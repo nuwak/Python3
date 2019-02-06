3f7b6b52eb4941a6fa67a813c0aef27c1c8515b3
Igor Bozhennikov <igor.bozhennikov@digitaledtech.com>
2019-02-06 19:39:16 +0300

    static type

---
+++ Python/Python3.7/static_type/links.md
@@ -0,0 +1,1 @@
+[How to Use Static Type Checking in Python 3.6](https://medium.com/@ageitgey/learn-how-to-use-static-type-checking-in-python-3-6-in-10-minutes-12c86d72677b)
---
+++ Python/Python3.7/static_type/new_type.py
@@ -0,0 +1,15 @@
+from typing import NewType
+
+UserId = NewType('UserId', int)
+some_id = UserId(524313)
+
+
+def get_user_name(user_id: UserId) -> str:
+    return 'user_id: ' + str(user_id)
+
+
+# typechecks
+user_a = get_user_name(UserId(42351))
+
+# does not typecheck; an int is not a UserId
+user_b = get_user_name(-1)
